c8c5f58aabd610c3339ed1b3d8539bf338caefb4
shehanhasanga <shehanhasanga.15@cse.mrt.ac.lk>
2022-03-15 12:54:27 +0530

    Initial Commit

---
+++ Authenticate/AuthenticateApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthenticateApp.swift
+//  Authenticate
+//
+//  Created by shehan karunarathna on 2022-03-15.
+//
+
+import SwiftUI
+
+@main
+struct AuthenticateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
