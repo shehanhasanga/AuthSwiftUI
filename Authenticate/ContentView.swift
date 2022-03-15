c0aaa1064800d8bed4dde796261f205353466d57
shehanhasanga <shehanhasanga.15@cse.mrt.ac.lk>
2022-03-15 13:10:45 +0530

    added new files

--- Authenticate/ContentView.swift
+++ Authenticate/ContentView.swift
@@ -6,12 +6,56 @@
 //
 
 import SwiftUI
+import LocalAuthentication
 
 struct ContentView: View {
+  @State var auth:Bool = false
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+      NavigationView{
+        VStack{
+          Button{
+            authenticate()
+          }label: {
+            Text("Press")
+              .padding()
+              .padding(.horizontal)
+              .background(Color.blue)
+              .foregroundColor(.white)
+              .clipShape(Capsule())
+          }
+          NavigationLink(destination: SecondView(), isActive: $auth) { EmptyView() }
+        }
+
+
+      }
+
     }
+  func authenticate() {
+      let context = LAContext()
+      var error: NSError?
+
+      if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+          let reason = "Please authenticate yourself to unlock your places."
+
+          context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+
+              if success {
+                print("auth successed")
+                auth.toggle()
+              } else {
+                  // error
+              }
+          }
+      } else {
+          // no biometrics
+      }
+  }
+}
+
+struct SecondView : View{
+  var body: some View {
+    Text("Secon View")
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
